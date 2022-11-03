33247c691b5eb224963f27fd472db99a49b85ffa
Marlon Garcia <marlongarcia2008@gmail.com>
2022-11-03 13:22:23 -0700

    agregando carrito de compras

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,5 +1,23 @@
 class Product < ApplicationRecord
-    validates :title, :description, :price, presence:true
-    validates :price, numericality: { greater_than_or_equal_to: 0.0 }
-    validates :title, uniqueness: true
+  has_many :line_items
+
+  validates :title, :description, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.0 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+      with:
+      %r{\.(gif|jpg|png)\z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'
+      }
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+
+    private
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'No se puede eliminar hay items que están utilizando el producto')
+        throw :abort
+      end
+    end
 end
---
+++ depot/spec/models/product_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+# https://semaphoreci.com/community/tutorials/how-to-test-rails-models-with-rspec
+RSpec.describe Product, :type => :model do
+  subject { build(:product_1) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  describe "is not valid without a uniqueness title" do
+    before { create(:product_2) }
+
+    it  do
+      expect(subject).to_not be_valid
+    end
+  end
+
+  it "is not valid without a description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a price" do
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end
+end
